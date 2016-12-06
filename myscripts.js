6f13822106a85452ef700f6b188d6d61b213b964
Marios Georgiou <mariosge90@gmail.com>
2016-12-06 02:17:39 +0000

    Footer is done, login and registration
    -Completed a quick footer and share it with all pages
    -I changed the style on the Login and registration pages and made them
    more appealing and cognitive with CSS
    -Registration page —> JS check if password match

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,13 @@
 /* JS logo change onScroll starts here */
-            window.onscroll = function() {myFunction()};
+            window.onscroll = function () {myFunction(); };
             function myFunction() {
                 if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-                    document.getElementById("logochange").src="images/onlylogo.png";
-                }
+                    document.getElementById("logochange").src = "images/onlylogo.png";
+                } 
                 else if (document.body.scrollTop < 250 || document.documentElement.scrollTop < 250){
-                    document.getElementById("logochange").src="images/LogoV.png";
+                    document.getElementById("logochange").src = "images/LogoV.png";
                 }
             }
 /* JS logo change onScroll ends here */
+
+
